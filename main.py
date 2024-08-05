4d18043b799d0c5b4aa2a01e8f2887e27ff7ebb4
ciyagerkral <163952750+ciyagerkral@users.noreply.github.com>
2024-08-05 13:51:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# API anahtarınızı buraya yapıştırın
+API_KEY = '6981545815:AAHuOOT1uBZRP5dtnuLmQAm17b-7a5r8lF4'
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Merhaba! Ben sizin botunuzum.')
+
+def main():
+    updater = Updater(API_KEY)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
